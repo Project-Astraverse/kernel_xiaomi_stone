7e1ce55116cf0b9d2953a7ae7391ddb6db449874
Amrito | アンブロシア <bhabanidas431@gmail.com>
2025-10-26 18:42:48 +0530

    Rebrand to Starry Kernel

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@
 # Set Kernel Build Variables
 DEVICE_CODENAME="stone"  # Device codename (e.g., veux, garnet, etc.)
 DEVICE_NAME="POCO X5 5G/Redmi Note 12 5G/Note 12R Pro"          # Device Market name (e.g., POCO X4 PRO 5G)
-KERNEL_NAME="Paimon"    # Kernel name
+KERNEL_NAME="Starry"    # Kernel name
 KERNEL_DEFCONFIG="vendor/${DEVICE_CODENAME}_qgki_defconfig"
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
 FINAL_KERNEL_ZIP="${KERNEL_NAME}-Kernel-${DEVICE_CODENAME}-$(date '+%Y%m%d').zip"
@@ -45,8 +45,8 @@ else
 fi
 
 export ARCH=arm64
-export KBUILD_BUILD_HOST=$BUILD_HOSTNAME
-export KBUILD_BUILD_USER="Embraceurself"
+export KBUILD_BUILD_HOST="アムリトクン"
+export KBUILD_BUILD_USER="私は彼女を愛している"
 export KBUILD_COMPILER_STRING="$COMPILER_NAME"
 
 # Telegram Bot Config
