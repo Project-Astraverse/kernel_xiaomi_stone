c9b34dbf3184da9111dcfd2435517aaa766d0c7c
Amrito | アンブロシア <bhabanidas431@gmail.com>
2025-10-26 19:34:23 +0530

    arm64: configs: Rename defconfig

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@
 DEVICE_CODENAME="stone"  # Device codename (e.g., veux, garnet, etc.)
 DEVICE_NAME="POCO X5 5G/Redmi Note 12 5G/Note 12R Pro"           # Leave empty to set manually
 KERNEL_NAME="starry"    # Kernel name
-KERNEL_DEFCONFIG="${DEVICE_CODENAME}_qgki_defconfig"
+KERNEL_DEFCONFIG="${DEVICE_CODENAME}_defconfig"
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
 FINAL_KERNEL_ZIP="${KERNEL_NAME}-Kernel-${DEVICE_CODENAME}-$(date '+%Y%m%d').zip"
 BUILD_STATUS="STABLE"
